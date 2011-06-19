cd460dd40d4103f03275204904d0c506631c7914
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2011-06-19 21:57:19 +0200

    Write images when clicked

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ const int ENTER_KEYCODE = 13;
 int main(int, char**)
 {
    SmalfilmsGrabber grabber;
-   grabber.grabImage();
    grabber.main();
 }
 
--- smalfilmsgrabber.h
+++ smalfilmsgrabber.h
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <cstdio>
+#include <iomanip>
 
 #include "cv.h"
 #include "highgui.h"
@@ -12,47 +15,38 @@
 void onMouseClick(int event, int x, int y, int flags, void* param);
 
 class SmalfilmsGrabber {
+   cv::Mat frame_;
    cv::VideoCapture capturer_;
-   cv::VideoWriter writer_;
-   std::vector<cv::Mat> frames_;
+   int imageId_;
 public:
-   SmalfilmsGrabber() 
-      : capturer_(0)
-      , writer_("out.mov", CV_FOURCC('P','I','M','1'), capturer_.get(CV_CAP_PROP_FPS), cv::Size(capturer_.get(CV_CAP_PROP_FRAME_WIDTH), capturer_.get(CV_CAP_PROP_FRAME_WIDTH)) )  
-      //, writer_("out.avi", -1, 25, cv::Size(640, 480) )  
+   SmalfilmsGrabber() : 
+      capturer_(0),
+      imageId_(0)
+      
    {
       if(!capturer_.isOpened())
          throw std::runtime_error("Couldn't open default camera device!");
 
-      if (!writer_.isOpened())
-         throw std::runtime_error("Couldn't open video writer");
-
-      //cv::namedWindow("SmalfilmsGrabber",CV_WINDOW_AUTOSIZE);
-      //cv::setMouseCallback("SmalfilmsGrabber", onMouseClick, this);
-      frames_.resize(100);
-      std::cout << "Camera FPS: " << capturer_.get(CV_CAP_PROP_FPS) << std::endl;
-      std::cout << "Resolution: " << capturer_.get(CV_CAP_PROP_FRAME_WIDTH) << "x" << capturer_.get(CV_CAP_PROP_FRAME_WIDTH) << std::endl;
+      cv::namedWindow("SmalfilmsGrabber",CV_WINDOW_AUTOSIZE);
+      cv::setMouseCallback("SmalfilmsGrabber", onMouseClick, this);
    }
 
-   void grabImage() {
-      cv::Mat frame;
-      capturer_ >> frame;
-      //std::cout << "Caught frame" << std::endl;
-      //imshow("SmalfilmsGrabber", frame);
-      writer_ << frame;
+   void grabImage(bool show = false) {
+      using namespace std;
+      capturer_ >> frame_;
+      stringstream ss;
+      ss << "capture_" << setw(6) << setfill('0') << imageId_++ << ".jpg";
+      imwrite(ss.str(), frame_);
+      if (show) {
+         cout << "Caught frame" << endl;
+         imshow("SmalfilmsGrabber", frame_);
+      }
    }
 
    void main() {
-      for (int i=0; i<50; ++i) {
-         grabImage();
-      }
-      std::cout << "Done grabbing" << std::endl;
-
-      // while (cv::waitKey() < 0) {
-      //    if (i<frames_.size()) {
-      //       //capturer_ >> frames_[i];
-      //    }
-      // }
+      // Grab image to init "UI".
+      grabImage(1);
+      while (cv::waitKey() < 0) { }
    }
 };
 
